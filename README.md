977f5b6a07fd8b7ae9b23d7c2290203455131180
David Wotherspoon <david@dwotherspoon.com>
2023-05-21 13:40:12 +0300

    Init2

---
+++ README.md
@@ -0,0 +1,3 @@
+# MLib
+
+A suite of basic libraries for handling strings, printf and FAT filesystems, targetting microcontrollers.
---
+++ src/include/mlib/mfat_config.h
@@ -0,0 +1,6 @@
+/* Maximum number of volumes that can be mounted simultaneously */
+#define MFAT_MAX_VOLUMES        (4)
+
+/* Minimum and maximum supported sector sizes (increases memory footprint) */
+#define MFAT_MIN_SECTORSZ       (512)
+#define MFAT_MAX_SECTORSZ       (4096)
